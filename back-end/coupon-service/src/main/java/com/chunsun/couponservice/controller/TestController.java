9e5683a70b1a7173775f9328bf14da392d0df45b
유선우 <dbtjsdn980818@naver.com>
2025-02-03 16:49:08 +0900

    Feature/62 - Spring Cloud 관련 설정

--- back-end/coupon-service/src/main/java/com/chunsun/couponservice/controller/TestController.java
+++ back-end/coupon-service/src/main/java/com/chunsun/couponservice/controller/TestController.java
@@ -1,12 +1,25 @@
 package com.chunsun.couponservice.controller;
 
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+@RefreshScope
+@RequestMapping("/")
 @RestController
 public class TestController {
+
+	private final Environment environment;
+
 	@GetMapping
 	public String test() {
-		return "coupon-service";
+		System.out.println("==" + environment.getProperty("token.expiration_time"));
+		return environment.getProperty("token.expiration_time");
+		// return "coupon-service Test";
 	}
 }
