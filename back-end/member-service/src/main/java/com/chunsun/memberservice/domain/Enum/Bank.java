2cc263343b736897735aba217bb7c7e4365d23fc
김범준 <kbj7565@gmail.com>
2025-02-07 08:46:34 +0900

    Fix/51 Member Bank Enum 추가

--- back-end/member-service/src/main/java/com/chunsun/memberservice/domain/Enum/Bank.java
+++ back-end/member-service/src/main/java/com/chunsun/memberservice/domain/Enum/Bank.java
@@ -1,5 +1,27 @@
 package com.chunsun.memberservice.domain.Enum;
 
 public enum Bank {
-	KB, SHINHAN, WOORI
+	KB,            // KB국민은행
+	SC,            // SC제일은행
+	KYUNGNAM,      // 경남은행
+	GWANGJU,       // 광주은행
+	IBK,           // 기업은행
+	NONGHYUP,      // 농협
+	DAEGU,         // 대구은행
+	BUSAN,         // 부산은행
+	KDB,           // 산업은행
+	SAEMAUL,       // 새마을금고
+	SUHYUP,        // 수협
+	SHINHAN,       // 신한은행
+	SHINHYEOP,     // 신협
+	KEB,           // 외환은행
+	WOORI,         // 우리은행
+	POST,          // 우체국
+	JEONBUK,       // 전북은행
+	CHUKHYUP,      // 축협
+	KAKAO,         // 카카오뱅크
+	KEBANK,        // 케이뱅크
+	HANA,          // 하나은행
+	CITI,          // 한국씨티은행
+	TOSS           // 토스뱅크
 }
