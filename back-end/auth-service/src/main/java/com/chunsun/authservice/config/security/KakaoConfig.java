54939755ca76bc3acf2f6f1090e47fc8818952d3
문인규 <ansdlsrb1121@naver.com>
2025-02-08 16:03:45 +0900

    [production-BE] Feature/53 - 인증 로그 기입 및 불필요 변수 제거

--- back-end/auth-service/src/main/java/com/chunsun/authservice/config/security/KakaoConfig.java
+++ back-end/auth-service/src/main/java/com/chunsun/authservice/config/security/KakaoConfig.java
@@ -19,10 +19,6 @@ public class KakaoConfig {
 		return clientId;
 	}
 
-	public String getClientSecret() {
-		return clientSecret;
-	}
-
 	public String getRedirectUri() {
 		return redirectUri;
 	}
