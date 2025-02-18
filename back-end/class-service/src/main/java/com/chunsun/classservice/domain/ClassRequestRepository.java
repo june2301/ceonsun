7cab52dfab2f7fa5a955d1df12dc92a2e9485060
BrokenFinger98 <dbtjsdn980818@gmail.com>
2025-02-19 01:19:56 +0900

    fix: createClassRequestResponse api 수정

--- back-end/class-service/src/main/java/com/chunsun/classservice/domain/ClassRequestRepository.java
+++ back-end/class-service/src/main/java/com/chunsun/classservice/domain/ClassRequestRepository.java
@@ -9,6 +9,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 public interface ClassRequestRepository extends JpaRepository<ClassRequest, Long> {
 	Page<ClassRequest> findAllByTeacherId(Long teacherId, Pageable pageable);
 	Page<ClassRequest> findAllByStudentId(Long studentId, Pageable pageable);
-	Optional<ClassRequest> deleteByIdAndTeacherIdAndStudentId(Long classRequestId, Long teacherId, Long studentId);
+	Optional<Integer> deleteByIdAndTeacherIdAndStudentId(Long classRequestId, Long teacherId, Long studentId);
 	Optional<ClassRequest> findByTeacherIdAndStudentId(Long teacherId, Long studentId);
 }
