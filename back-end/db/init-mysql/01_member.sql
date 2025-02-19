81a85c8ba95831cd7c1d0c3502dd001b0a09f5d3
김범준 <kbj7565@gmail.com>
2025-02-19 23:28:34 +0900

    Fix/51 Member Teacher 스키마 변경 classProgress -> careerProgress

--- back-end/db/init-mysql/01_member.sql
+++ back-end/db/init-mysql/01_member.sql
@@ -1,5 +1,23 @@
 USE member;
 
+CREATE TABLE teachers (
+                          id                   BIGINT UNSIGNED NOT NULL,
+                          description          TEXT DEFAULT NULL,
+                          career_description   TEXT DEFAULT NULL,
+                          career_progress      TEXT DEFAULT NULL,
+                          class_contents       TEXT DEFAULT NULL,
+                          total_class_count    INT NOT NULL DEFAULT 0,
+                          total_class_hours    INT NOT NULL DEFAULT 0,
+                          is_wanted            BOOLEAN NOT NULL DEFAULT TRUE,
+                          bank                 ENUM(
+        'KB', 'SC', 'KYUNGNAM', 'GWANGJU', 'IBK', 'NONGHYUP', 'DAEGU', 'BUSAN',
+        'KDB', 'SAEMAUL', 'SUHYUP', 'SHINHAN', 'SHINHYEOP', 'KEB', 'WOORI',
+        'POST', 'JEONBUK', 'CHUKHYUP', 'KAKAO', 'KEBANK', 'HANA', 'CITI', 'TOSS'
+    ) NOT NULL,
+                          account              VARCHAR(20) NOT NULL,
+                          price                INT NOT NULL DEFAULT 10000
+);
+
 CREATE TABLE members (
     id            BIGINT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
     kakao_id      VARCHAR(255) NOT NULL UNIQUE,
@@ -13,24 +31,6 @@ CREATE TABLE members (
     created_at    DATETIME     NOT NULL DEFAULT CURRENT_TIMESTAMP,
     updated_at    DATETIME     NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
     deleted_at    DATETIME     DEFAULT NULL
-);
-
-CREATE TABLE teachers (
-    id                   BIGINT UNSIGNED NOT NULL,
-    description          TEXT DEFAULT NULL,
-    career_description   TEXT DEFAULT NULL,
-    class_contents       TEXT DEFAULT NULL,
-    class_progress       TEXT DEFAULT NULL,
-    total_class_count    INT NOT NULL DEFAULT 0,
-    total_class_hours    INT NOT NULL DEFAULT 0,
-    is_wanted            BOOLEAN NOT NULL DEFAULT TRUE,
-    bank                 ENUM(
-        'KB', 'SC', 'KYUNGNAM', 'GWANGJU', 'IBK', 'NONGHYUP', 'DAEGU', 'BUSAN',
-        'KDB', 'SAEMAUL', 'SUHYUP', 'SHINHAN', 'SHINHYEOP', 'KEB', 'WOORI',
-        'POST', 'JEONBUK', 'CHUKHYUP', 'KAKAO', 'KEBANK', 'HANA', 'CITI', 'TOSS'
-    ) NOT NULL,
-    account              VARCHAR(20) NOT NULL,
-    price                INT NOT NULL DEFAULT 10000
 );
 
 CREATE TABLE students (
