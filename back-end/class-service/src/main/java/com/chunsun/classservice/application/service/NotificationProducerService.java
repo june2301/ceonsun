25e85e51427fec38516993f332cd381341ad3278
황인준 <os2864@naver.com>
2025-02-20 18:57:18 +0900

    fix: Producer 코드 변경 및 로그 출력

--- back-end/class-service/src/main/java/com/chunsun/classservice/application/service/NotificationProducerService.java
+++ back-end/class-service/src/main/java/com/chunsun/classservice/application/service/NotificationProducerService.java
@@ -15,18 +15,10 @@ import reactor.core.publisher.Mono;
 public class NotificationProducerService {
 	private final KafkaTemplate<String, RequestDto> kafkaTemplate;
 
-	public Mono<String> sendNotification(RequestDto requestDto) {
-		return Mono.create(sink -> {
-			kafkaTemplate.send("alarm", requestDto)
-				.whenComplete((result, ex) -> {
-					if (ex == null) {
-						log.info("Kafka 메시지 전송 성공: {}", result.getProducerRecord().value());
-						sink.success("SUCCESS");
-					} else {
-						log.error("Kafka 메시지 전송 실패: {}", ex.getMessage());
-						sink.success("FAILURE");
-					}
-				});
-		});
+	public void sendNotification(RequestDto requestDto) {
+		log.info(requestDto.getSendUserId());
+		log.info(requestDto.getTargetUserId());
+
+		kafkaTemplate.send("alarm", requestDto);
 	}
 }
